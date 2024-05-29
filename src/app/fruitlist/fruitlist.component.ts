811e2b8efc70b2bbbe7e6611f8376808858db085
PhiLiNNN <philipp_wendschuch@web.de>
2024-05-30 01:53:49 +0200

    first angular  project some tryouts

---
+++ src/app/fruitlist/fruitlist.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
+import { FruitlistdataService } from '../fruitlistdata.service';
+
+@Component({
+  selector: 'app-fruitlist',
+  standalone: true,
+  imports: [CommonModule, SinglefruitComponent],
+  templateUrl: './fruitlist.component.html',
+  styleUrl: './fruitlist.component.scss',
+})
+export class FruitlistComponent {
+  fruitlistdata = inject(FruitlistdataService);
+
+  addComment(comment: string, index: number) {
+    this.fruitlistdata.addCommentToFruit(comment, index);
+  }
+}
